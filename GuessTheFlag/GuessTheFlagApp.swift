4963a4f780e4e38a23f4d3bc2faa066a735ab249
Diana Dashynevich <ddashinevich@gmail.com>
2024-12-09 11:46:32 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Diana Dashinevich on 09/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
