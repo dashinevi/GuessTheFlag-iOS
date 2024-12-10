987a87da4dc5425218e0af4be99ea24ef17102f7
Diana Dashynevich <ddashinevich@gmail.com>
2024-12-10 13:00:42 +0100

    feat: limit game to 8 questions and add final score alert

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -14,20 +14,26 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
+    @State private var selectedFlag = 0
+    @State private var questionCount = 1
+    @State private var showingFinalScore = false
     
     var body: some View {
         ZStack {
-            Color.indigo
+            LinearGradient(colors: [.indigo, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
-            VStack(spacing: 30) {
+            VStack(spacing: 25) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundStyle(.white)
-                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .font(.title).bold()
+                        .fontDesign(.rounded)
+                    
                     Text(countries[correctAnswer])
                         .foregroundStyle(.white)
-                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                        .font(.largeTitle).bold()
+                        .fontDesign(.rounded)
                 }
                 
                 ForEach(0..<3) { number in
@@ -35,28 +41,57 @@ struct ContentView: View {
                         flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                 }
+                Text("Your score is \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title).bold()
+                    .fontDesign(.rounded)
             }
         }
         .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
+            Button("Continue", action: nextQuestion)
         } message: {
-            Text("Your score is \(score)")
+            if selectedFlag != correctAnswer {
+                Text("That’s the flag of \(countries[selectedFlag])")
+            } else {
+                Text("Good job!")
+            }
+        }
+        .alert("Game Over", isPresented: $showingFinalScore) {
+            Button("Restart", action: restartGame)
+        } message: {
+            Text("Your final score is \(score) out of 8.")
         }
     }
     
     func flagTapped(_ number: Int) {
-            if number == correctAnswer {
-                scoreTitle = "Correct"
-                score += 1
-            } else {
-                scoreTitle = "Wrong"
-            }
+        selectedFlag = number
+        if selectedFlag == correctAnswer {
+            scoreTitle = "Correct!"
+            score += 1
+        } else {
+            scoreTitle = "Wrong!"
+        }
+        
+        if questionCount == 8 {
+            showingFinalScore = true
+        } else {
             showingScore = true
         }
+    }
     
-    func askQuestion() {
+    func nextQuestion() {
+        questionCount += 1
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func restartGame() {
+        score = 0
+        questionCount = 1
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
